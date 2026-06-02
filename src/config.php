196598c17dfd3804c859688e421d987597eca0c7
zekiyeilman <148627932+zekiyeilman@users.noreply.github.com>
2026-06-02 21:14:50 +0300

    Switch to stateless Cookie-based CSRF protection

--- src/config.php
+++ src/config.php
@@ -58,17 +58,19 @@ function getDB() {
 }
 
 function generate_csrf_token() {
-    if (empty($_SESSION['csrf_token'])) {
-        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    if (empty($_COOKIE['csrf_token'])) {
+        $token = bin2hex(random_bytes(32));
+        setcookie('csrf_token', $token, time() + 86400, '/');
+        $_COOKIE['csrf_token'] = $token;
     }
-    return $_SESSION['csrf_token'];
+    return $_COOKIE['csrf_token'];
 }
 
 function verify_csrf_token($token) {
-    if (!isset($_SESSION['csrf_token']) || empty($token)) {
+    if (empty($_COOKIE['csrf_token']) || empty($token)) {
         return false;
     }
-    return hash_equals($_SESSION['csrf_token'], $token);
+    return hash_equals($_COOKIE['csrf_token'], $token);
 }
 
 function is_post_request() {
